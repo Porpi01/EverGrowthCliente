79708df6fa7c3157bc07f51e1f49f3f19b842ba1
macanyiz@gmail.com <macanyiz@gmail.com>
2024-05-11 21:21:56 +0200

    Mayusculas registro

--- EverGrowthCliente/src/app/components/Shared/Register/Register.component.ts
+++ EverGrowthCliente/src/app/components/Shared/Register/Register.component.ts
@@ -1,10 +1,21 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CryptoService } from 'src/app/service/Crypto.service';
 import { UsuarioService } from 'src/app/service/Usuario.service';
+
+export function startWithCapitalLetter(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const value: string = control.value;
+    if (value && value.charAt(0) !== value.charAt(0).toUpperCase()) {
+      return { 'startWithCapitalLetter': { value: control.value } };
+    }
+    return null;
+  };
+}
+
 
 @Component({
   selector: 'app-Register',
@@ -26,12 +37,12 @@ export class RegisterComponent implements OnInit {
   ) {
     this.registerForm = this.fb.group({
     
-      nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
-      apellido1: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
-      apellido2: [ '', [Validators.minLength(3), Validators.maxLength(255)]],
+      nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255),startWithCapitalLetter()]],
+      apellido1: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255),startWithCapitalLetter()]],
+      apellido2: [ '', [Validators.minLength(3), Validators.maxLength(255),startWithCapitalLetter()]],
       email: ['', [Validators.required, Validators.email]],
       telefono: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('^[0-9]+$')]],
-      direccion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(255)]],
+      direccion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(255),startWithCapitalLetter()]],
       username: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15), Validators.pattern('^[a-zA-Z0-9]+$')]],
       password: ['', [Validators.required, Validators.minLength(6)]] // Agrega validación para la contraseña
     });
